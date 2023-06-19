e79c34f4d44c7d127e2479f566aff4b2072e331e
Daichi Tomaru <banaoa7543@gmail.com>
2023-06-19 09:48:17 +0900

    refactored 10

--- 10.py
+++ 10.py
@@ -1,3 +1,4 @@
+# first answer
 import math
 
 class Solution:
@@ -16,3 +17,16 @@ class Solution:
         continue
       max_index = marker_index
     return max_index
+
+# refactored
+class Solution:
+  def searchInsert(self, nums: List[int], target: int) -> int:
+    max_index = len(nums) - 1
+    min_index = 0
+    while max_index >= min_index:
+      marker_index = math.floor((max_index + min_index) / 2)
+      marker = nums[marker_index]
+      if target == marker: return marker_index
+      if target > marker:  min_index = marker_index + 1
+      else:                max_index = marker_index - 1
+    return min_index
